c433f126f50a3e8e70b3b26eabb7b76e2ad6c5eb
root <onyiede4@gmail.com>
2022-10-16 13:39:34 -0700

    alx if/else/while task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point to the program.
+ *
+ * Return: 0 if no errors, non-zero values if errors.
+ */
+int main (void)
+{
+	char i;
+
+	for (i = 'a', i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
