19c49bb098637a43249e8453ccb00504d47b90a5
root <onyiede4@gmail.com>
2022-10-21 16:00:17 -0700

    more nested loops 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  *
+ * Return: void
  */
 
 void print_numbers(void)
@@ -10,8 +11,6 @@ void print_numbers(void)
 	char c;
 
 	for (c = '0'; c >= '9'; c++)
-	{
 		_putchar(c);
-	}
 	_putchar('\n');
 }
