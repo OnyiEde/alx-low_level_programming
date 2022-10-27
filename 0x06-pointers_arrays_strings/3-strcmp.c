76c1fb44e70ef52fbdf740499f02e311544394fd
root <onyiede4@gmail.com>
2022-10-27 15:44:59 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings<F11><F11>
+ * @s1: 1st string
+ * @s2: second string
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
