f7fb54ddab7de8e1d627ff2054ff139c27f0d25b
root <onyiede4@gmail.com>
2022-11-03 12:17:48 -0700

    advanced task 2

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,35 @@
 #include "main.h"
 
 /**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
  *
+ * s2 can contain the special character *
+ * The special char * can replace any string
+ * (including an empty string)
  *
+ * Return: 1 if the strings can be considered identical
+ * 0 if otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
