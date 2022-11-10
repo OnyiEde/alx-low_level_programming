99c6fdfa6f519ea004c2a02de8532d7de8b48468
root <onyiede4@gmail.com>
2022-11-09 20:58:59 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -43,7 +43,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
-	for (j = p; j < len2; j++, i++)
+	for (j = 0; j < len2; j++, i++)
 		result[i] = s2[j];
 	result[len] = '\0';
 
