1e236a1bc671fe35fd37d2ad28e02cb2bdaf4b73
root <onyiede4@gmail.com>
2022-11-15 11:02:23 -0800

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This folder is for structure typedef tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - class of the dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+#endif
